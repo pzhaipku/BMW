c48fc0b596544fc1c756eeea885bf507db3c0c3c
Mert9537 <mrtozn1923@gmail.com>
2016-12-10 20:17:23 +0300

    AdminV1

---
+++ BMW/BMW/Giris.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMW
+{
+    public partial class Giris : Form
+    {
+        SQL cumle = new SQL();
+        AdminPanel admin = new AdminPanel();
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Kapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Giris_Click(object sender, EventArgs e)
+        {
+            cumle.Select("Select*from Kullanici where Kullanici_adi='"+txt_Kulad.Text.ToString()+"' AND Kullanici_sifre='"+txt_Sifre.Text.ToString()+"'");
+            if (cumle.tablo.Rows.Count > 0)
+            {
+                admin.Show();
+                this.Hide();
+            }
+            else { MessageBox.Show("Hatalı Giriş"); }
+        }
+    }
+}
---
+++ BMW/BMW/SQL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BMW
+{
+    class SQL
+    {
+      
+        SqlConnection bag_cumle = new SqlConnection( "Data Source=OZEN; Initial Catalog=BMW;Integrated Security=true");
+        SqlDataAdapter adapter;
+        public DataTable tablo=new DataTable();
+        private void baglan(int secim)
+        {
+            if (secim == 1)
+            {
+                if (bag_cumle.State == ConnectionState.Closed)
+                {
+                    bag_cumle.Open();
+                }
+            }
+            else if (secim == 0)
+            {
+                bag_cumle.Close();
+            }
+        }
+        public void Select(string sorgu)
+        {
+            baglan(1);
+            adapter = new SqlDataAdapter(sorgu,bag_cumle);
+            adapter.Fill(tablo);
+            baglan(0);
+        }
+    }
+}

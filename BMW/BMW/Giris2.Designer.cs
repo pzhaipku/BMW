55b6a0a305434e2e4caf3b4ffee00c342819ec81
firatdrsn <drsn1995@gmail.com>
2016-12-18 19:05:12 +0300

    çalışan hali

--- BMW/BMW/Giris2.Designer.cs
+++ BMW/BMW/Giris2.Designer.cs
@@ -28,178 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris2));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.txt_Kulad = new System.Windows.Forms.TextBox();
-            this.txt_Sifre = new System.Windows.Forms.TextBox();
-            this.btn_Giris = new System.Windows.Forms.Button();
-            this.btn_Kapat = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(89, 49);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 80);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // txt_Kulad
-            // 
-            this.txt_Kulad.Location = new System.Drawing.Point(102, 149);
-            this.txt_Kulad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.txt_Kulad.Name = "txt_Kulad";
-            this.txt_Kulad.Size = new System.Drawing.Size(173, 21);
-            this.txt_Kulad.TabIndex = 1;
-            // 
-            // txt_Sifre
-            // 
-            this.txt_Sifre.Location = new System.Drawing.Point(102, 189);
-            this.txt_Sifre.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.txt_Sifre.Name = "txt_Sifre";
-            this.txt_Sifre.PasswordChar = '*';
-            this.txt_Sifre.Size = new System.Drawing.Size(173, 21);
-            this.txt_Sifre.TabIndex = 2;
-            // 
-            // btn_Giris
-            // 
-            this.btn_Giris.BackColor = System.Drawing.Color.Transparent;
-            this.btn_Giris.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Giris.BackgroundImage")));
-            this.btn_Giris.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Giris.FlatAppearance.BorderSize = 0;
-            this.btn_Giris.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.btn_Giris.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.btn_Giris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Giris.Location = new System.Drawing.Point(158, 237);
-            this.btn_Giris.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btn_Giris.Name = "btn_Giris";
-            this.btn_Giris.Size = new System.Drawing.Size(60, 60);
-            this.btn_Giris.TabIndex = 3;
-            this.btn_Giris.UseVisualStyleBackColor = false;
-            this.btn_Giris.Click += new System.EventHandler(this.btn_Giris_Click);
-            // 
-            // btn_Kapat
-            // 
-            this.btn_Kapat.BackColor = System.Drawing.Color.Transparent;
-            this.btn_Kapat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Kapat.BackgroundImage")));
-            this.btn_Kapat.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Kapat.FlatAppearance.BorderSize = 0;
-            this.btn_Kapat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.btn_Kapat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.btn_Kapat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Kapat.Location = new System.Drawing.Point(311, 340);
-            this.btn_Kapat.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btn_Kapat.Name = "btn_Kapat";
-            this.btn_Kapat.Size = new System.Drawing.Size(60, 60);
-            this.btn_Kapat.TabIndex = 4;
-            this.btn_Kapat.UseVisualStyleBackColor = false;
-            this.btn_Kapat.Click += new System.EventHandler(this.btn_Kapat_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(66, 143);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(65, 186);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox3.TabIndex = 6;
-            this.pictureBox3.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 237);
+            this.button1.Location = new System.Drawing.Point(47, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Servis";
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(35, 247);
+            this.button2.Location = new System.Drawing.Point(177, 76);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Araç Satis";
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(177, 336);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // Giris2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(384, 412);
-            this.ControlBox = false;
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.btn_Kapat);
-            this.Controls.Add(this.btn_Giris);
-            this.Controls.Add(this.txt_Sifre);
-            this.Controls.Add(this.txt_Kulad);
-            this.Controls.Add(this.pictureBox1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Giris2";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = " ";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.Text = "Giris2";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox txt_Kulad;
-        private System.Windows.Forms.TextBox txt_Sifre;
-        private System.Windows.Forms.Button btn_Giris;
-        private System.Windows.Forms.Button btn_Kapat;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
-
--- BMW/BMW/Giris2.cs
+++ BMW/BMW/Giris2.cs
@@ -12,37 +12,15 @@ namespace BMW
 {
     public partial class Giris2 : Form
     {
-        SQL2 cumle = new SQL2();
         public Giris2()
         {
             InitializeComponent();
         }
 
-        private void btn_Kapat_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btn_Giris_Click(object sender, EventArgs e)
-        {
-            cumle.Select("Select*from Kullanici where Kullanici_adi='"+txt_Kulad.Text.ToString()+"' AND Kullanici_sifre='"+txt_Sifre.Text.ToString()+"'");
-            if (cumle.tablo.Rows.Count > 0)
-            {
-                this.Hide();
-            }
-            else { MessageBox.Show("Hatalı Giriş"); }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            Servis srv_form = new Servis();
-            srv_form.Show();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Arac_Satis asatisfrm = new Arac_Satis();
-            asatisfrm.Show();
+            Servis srvs = new Servis();
+            srvs.Show();
         }
     }
 }
